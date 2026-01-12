1a9b6a57a809cc10375c569b8a1db51e28676b8a
YourDevVince <vince.dev.help@gmail.com>
2026-01-12 00:06:07 -0600

    initial commit

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import './Header.css';
+
+function Header() {
+  return (
+    <div className='Header'>
+      <div className='header__container'></div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { fileURLToPath, URL } from 'node:url';
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: process.env.VITE_DEPLOY_BASE || '/',
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+});
